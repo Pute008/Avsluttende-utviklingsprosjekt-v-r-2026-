7b07ff45ab7dd1eaf2c1543701f7ed3fcf00c2e1
Pute008 <felixwestby@outlook.com>
2026-03-25 14:22:14 +0100

    added more files
    added index, css and js
    written more code in app.js

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const session = require("express-session");
+const app = express();
 
 const Database = require("better-sqlite3");
 
 const cors = require("cors");
 app.use(cors());
 
-const cors = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
 app.use(express.static('public'))
 
-app.use(express.json);
+app.use(express.json());
 
 const port = 3000
+
+app.use(
+    session({
+        secret: "hemmeligNøkkel",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    })
+);
+
+function kreverInnlogging(req, res, next) {
+    if(!req.session.user) {
+        return res.redirect('/index.html')
+    }
+}
 
 app.get('/', kreverInnlogging, (req, res) => {
 
